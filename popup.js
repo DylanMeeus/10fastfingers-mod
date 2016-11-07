4ab95c5dde37864d7a5cf9055473e42a208ca974
Dylan Meeus <meeusdylan@hotmail.com>
2016-11-07 22:31:59 +0100

    block speedtracker

--- popup.js
+++ popup.js
@@ -1,5 +1,11 @@
 function disableTimer(){
+    try{
     document.getElementById("timer").style.display = "none";
+    }catch(err){}
+
+    try{
+	document.getElementById("speed-tracker").style.display = "none"; }
+    catch(err){}
 }
 
 disableTimer();
