7c2a4cbb61004ea4a1b67ea9303ca31b6f0a709b
Dylan Meeus <meeusdylan@hotmail.com>
2017-02-11 00:34:01 +0100

    remove gosu chat window

--- popup.js
+++ popup.js
@@ -2,10 +2,14 @@ function disableTimer(){
     try{
     document.getElementById("timer").style.display = "none";
     }catch(err){}
-
     try{
 	document.getElementById("speed-tracker").style.display = "none"; }
     catch(err){}
+
+    // remove the gosu chat window.
+    var gosu = document.getElementById("gosu-embedded-chat-frame");
+    gosu.parentNode.removeChild(gosu);
+    
 }
 
 disableTimer();
